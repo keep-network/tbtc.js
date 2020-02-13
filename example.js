d728b29d2acc6edd735fc73c83dcdf6190605dcc
Antonio Salazar Cardozo <antonio@thesis.co>
2020-02-13 10:13:45 -0500

    Clean up example JS functionality
    The JS now runs through getting a Bitcoin address (which is incomplete
    functionality), and properly exits the process when the main example
    function finishes, either with error code 1 if there is an issue, or
    error code 0 if it completes normally.

--- example.js
+++ example.js
@@ -1,32 +1,38 @@
 import Web3 from 'web3';
 import TBTC from './src/TBTC.js';
 
-const provider = new Web3(
-    new Web3.providers.HttpProvider(
-        "https://ropsten.infura.io/v3/59fb36a36fa4474b890c13dd30038be5",
-        0,
-        ":e18ef5ef295944928dd87411bc678f19",
-    )
-);
+import HDWalletProvider from '@truffle/hdwallet-provider'
+const mnemonic = "egg dune news grocery detail frog kiwi hidden tuna noble speak over"
 
-const web3 = new Web3(provider)
+const provider = new HDWalletProvider(
+    mnemonic,
+    "https://:e18ef5ef295944928dd87411bc678f19@ropsten.infura.io/v3/59fb36a36fa4474b890c13dd30038be5",
+)
 
-async function booyan() {
-const tbtc = TBTC.configure({
-    web3: new Web3(provider),
-    bitcoinNetwork: "testnet",
-})
+async function runExample() {
+    const web3 = new Web3(provider)
+    web3.eth.defaultAccount = (await web3.eth.getAccounts())[0]
 
-const DepositFactory = await tbtc.DepositFactory
-const lotSizes = await DepositFactory.availableSatoshiLotSizes()
-console.log("available", lotSizes.map(_ => _.toString()))
+    const tbtc = TBTC.configure({
+        web3: web3,
+        bitcoinNetwork: "testnet",
+    })
 
-const deposit = await DepositFactory.withSatoshiLotSize(lotSizes[0])
-console.log("deposit", deposit)
+    const DepositFactory = await tbtc.DepositFactory
+    const lotSizes = await DepositFactory.availableSatoshiLotSizes()
+
+    const deposit = await DepositFactory.withSatoshiLotSize(lotSizes[0])
+    const fundingAddress = await deposit.getBitcoinAddress()
+    console.log("deposit funding address:", fundingAddress)
+
 }
 
-booyan()
-    .then(() => console.log("SHAMAN"))
+runExample()
+    .then(() => {
+        console.log("All done!")
+        process.exit(0)
+    })
     .catch((error) => {
-        console.log("Boom boom time", error)
+        console.error("Boom boom time", error)
+        process.exit(1)
     })
