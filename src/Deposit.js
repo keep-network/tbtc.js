6addadacf9a1e31a10651a237056d1cc5a12ef0a
Antonio Salazar Cardozo <antonio@thesis.co>
2020-02-13 16:22:35 -0500

    Implement Redemption.onBitcoinTransactionSigned

--- src/Deposit.js
+++ src/Deposit.js
@@ -1504,7 +1504,7 @@ class Redemption {
     }
 
     onBitcoinTransactionSigned(transactionHandler/*: (transaction)=>void*/) {
-
+        this.signedTransaction.then(transactionHandler)
     }
 
     onWithdrawn(withdrawalHandler/*: (txHash)=>void*/) { // bitcoin txHash
