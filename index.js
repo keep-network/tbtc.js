9c38a5b273238436946f2a3c590d8c066b19cf72
Antonio Salazar Cardozo <antonio@thesis.co>
2020-02-15 15:32:01 -0500

    Export Bitcoin and Ethereum helpers

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import TBTC from './src/TBTC.js';
+import BitcoinHelpers from './src/BitcoinHelpers.js';
+import EthereumHelpers from './src/EthereumHelpers.js';
+
+export const BitcoinHelpers = BitcoinHelpers
+export const EthereumHelpers = EthereumHelpers
 
 export default TBTC
