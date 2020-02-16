620789c26a589f3e859e2db004ddfa9d0fe8f83d
Antonio Salazar Cardozo <antonio@thesis.co>
2020-02-15 22:40:54 -0500

    Fix helpers export, bump to 0.6.10

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import TBTC from './src/TBTC.js';
 import BitcoinHelpers from './src/BitcoinHelpers.js';
 import EthereumHelpers from './src/EthereumHelpers.js';
 
-export const BitcoinHelpers = BitcoinHelpers
-export const EthereumHelpers = EthereumHelpers
+export { BitcoinHelpers, EthereumHelpers }
 
 export default TBTC
