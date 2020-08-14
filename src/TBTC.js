93a27b8a2f5f0bc8f19e4cd79d7e27fa3d6b115d
Antonio Salazar Cardozo <antonio@thesis.co>
2020-08-14 09:43:52 -0400

    Rework TBTC initialization a little
    Specifically, move to using EthereumHelpers.isMainnet. Notably, this is
    an async function rather than a regular one, so the network match check
    has to be performed in the static factory function rather than in the
    constructor (which cannot be async).
    
    This moves us overall to using the blessed `web3.eth.getChainId` path to
    get the current chain id, rather than the previous
    `.currentProvider.chainId` solution which was not part of the public
    Web3 API.

--- src/TBTC.js
+++ src/TBTC.js
@@ -1,9 +1,14 @@
-import { DepositFactory } from "./Deposit.js"
-import BitcoinHelpers from "./BitcoinHelpers.js"
 import BN from "bn.js"
+/** @typedef { import("web3").default } Web3 */
+
+import BitcoinHelpers from "./BitcoinHelpers.js"
+/** @typedef { import("./BitcoinHelpers.js").BitcoinNetwork } BitcoinNetwork */
+import EthereumHelpers from "./EthereumHelpers.js"
+
+import { DepositFactory } from "./Deposit.js"
 import { Constants } from "./Constants.js"
+
 import TBTCSystemJSON from "@keep-network/tbtc/artifacts/TBTCSystem.json"
-/** @typedef { import("./BitcoinHelpers.js").BitcoinNetwork } BitcoinNetwork
 
 /**
  * @typedef {Object} TBTCConfig
@@ -11,55 +16,34 @@ import TBTCSystemJSON from "@keep-network/tbtc/artifacts/TBTCSystem.json"
  * @prop {Web3} web3
  */
 
-/** @type {TBTCConfig} */
-const defaultConfig = {
-  bitcoinNetwork: BitcoinHelpers.Network.TESTNET,
-  web3: global.Web3
-}
-
 /**
- * @param {Web3} web3 The web3 instance
- * @return {boolean} True if the web3 instance is aimed at Ethereum mainnet,
- *         false otherwise.
+ * The entry point to the TBTC system. Call `TBTC.withConfig()` and pass the
+ * appropriate configuration object with web3, Bitcoin network, and Electrum
+ * information to receive an initialized instance of the `TBTC` class. The class
+ * then exposes two properties, `Deposit` and `Constants`. `Deposit` is a
+ * factory object for looking up and creating deposits, while `Constants` allows
+ * direct access to tBTC system constants.
  */
-function isMainnet(web3) {
-  return web3.currentProvider["chainId"] == 0x1
-}
-/**
- * @param {Web3} web3 The web3 instance
- * @return {boolean} True if the web3 instance is aimed at an Ethereum testnet,
- *         false otherwise.
- */
-function isTestnet(web3) {
-  return !isMainnet(web3)
-}
-
 export class TBTC {
-  static async withConfig(config = defaultConfig, networkMatchCheck = true) {
-    const depositFactory = await DepositFactory.withConfig(config)
-    const constants = await Constants.withConfig(config)
-    return new TBTC(depositFactory, constants, config, networkMatchCheck)
-  }
-
   /**
    *
-   * @param {DepositFactory} depositFactory
-   * @param {Constants} constants
-   * @param {TBTCConfig} config
-   * @param {boolean} networkMatchCheck
+   * @param {TBTCConfig} config The configuration to use for this instance.
+   * @param {boolean} [networkMatchCheck=true] When true, ensures that the
+   *        configured Bitcoin and Ethereum networks are either both mainnet or
+   *        both testnet.
    */
-  constructor(depositFactory, constants, config, networkMatchCheck = true) {
+  static async withConfig(config, networkMatchCheck = true) {
+    const ethereumMainnet = await EthereumHelpers.isMainnet(config.web3)
+    const bitcoinMainnet =
+      config.bitcoinNetwork == BitcoinHelpers.Network.MAINNET
+
     if (
       networkMatchCheck &&
-      ((isMainnet(config.web3) &&
-        config.bitcoinNetwork == BitcoinHelpers.Network.TESTNET) ||
-        (isMainnet(config.web3) &&
-          config.bitcoinNetwork == BitcoinHelpers.Network.SIMNET) ||
-        (isTestnet(config.web3) &&
-          config.bitcoinNetwork == BitcoinHelpers.Network.MAINNET))
+      ((ethereumMainnet && !bitcoinMainnet) ||
+        (!ethereumMainnet && bitcoinMainnet))
     ) {
       throw new Error(
-        `Ethereum network ${config.web3.currentProvider.chainId} ` +
+        `Ethereum network ${await config.web3.eth.getChainId()} ` +
           `and Bitcoin network ${config.bitcoinNetwork} are not both ` +
           `on testnet or both on  mainnet. Quitting while we're ` +
           `ahead. Developers can also pass false as the ` +
@@ -67,6 +51,18 @@ export class TBTC {
       )
     }
 
+    const depositFactory = await DepositFactory.withConfig(config)
+    const constants = await Constants.withConfig(config)
+    return new TBTC(depositFactory, constants, config)
+  }
+
+  /**
+   *
+   * @param {DepositFactory} depositFactory
+   * @param {Constants} constants
+   * @param {TBTCConfig} config
+   */
+  constructor(depositFactory, constants, config) {
     /** @package */
     this.depositFactory = depositFactory
     /** @package */
