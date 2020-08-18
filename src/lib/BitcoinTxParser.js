021c2c5388ea76a9654e7cc2a9816e35c2e629f5
Antonio Salazar Cardozo <antonio@thesis.co>
2020-08-18 15:19:45 -0400

    Add types to BitcoinTxParser.js
    There were types missing throughout, and the only thing that was
    allowing this file to pass through unscathed was permitting implicit any
    types.

--- src/lib/BitcoinTxParser.js
+++ src/lib/BitcoinTxParser.js
@@ -2,8 +2,32 @@ import BcoinPrimitives from "bcoin/lib/primitives/index.js"
 const { TX } = BcoinPrimitives
 import bufio from "bufio"
 
+/** @typedef { import('bcoin/lib/primitives/tx.js') } TX */
+/** @typedef { import('bufio').StaticWriter } StaticWriter */
+/** @typedef { import('bufio').BufferWriter } BufferWriter */
+
+/**
+ * @typedef {object} TransactionData
+ * @property {string} version The transaction's version field as an unprefixed
+ *           hexadecimal string.
+ * @property {string} txInVector The transaction's input vector as an unprefixed
+ *           hexadecimal string.
+ * @property {string} txOutVector The transaction's output vector as an
+ *           unprefixed hexadecimal string.
+ * @property {string} locktime The transaction's locktime field as an unprefixed
+ *           hexadecimal string.
+ */
+
+/**
+ * Parses the given raw transaction data to a set of fields extracted from that
+ * transaction.
+ *
+ * @param {string} rawTx Raw transaction data as an unprefixed hexadecimal
+ *        string.
+ * @return {TransactionData} The deserialized transaction data.
+ */
 function parse(rawTx) {
-  const tx = TX.fromRaw(rawTx, "hex")
+  const tx = TX.fromRaw(Buffer.from(rawTx, "hex"), null)
 
   return {
     version: getTxVersion(tx),
@@ -13,6 +37,10 @@ function parse(rawTx) {
   }
 }
 
+/**
+ * @param {TX} tx The bcoin transaction to pull the version from.
+ * @return {string} The transaction version as an unprefixed hexadecimal string.
+ */
 function getTxVersion(tx) {
   const buffer = bufio.write()
   buffer.writeU32(tx.version)
@@ -20,14 +48,29 @@ function getTxVersion(tx) {
   return toHex(buffer)
 }
 
+/**
+ * @param {TX} tx The bcoin transaction to pull the input vector from.
+ * @return {string} The transaction input vector as an unprefixed hexadecimal
+ *         string.
+ */
 function getTxInputVector(tx) {
   return vectorToRaw(tx.inputs)
 }
 
+/**
+ * @param {TX} tx The bcoin transaction to pull the output vector from.
+ * @return {string} The transaction output vector as an unprefixed hexadecimal
+ *         string.
+ */
 function getTxOutputVector(tx) {
   return vectorToRaw(tx.outputs)
 }
 
+/**
+ * @param {TX} tx The bcoin transaction to pull the locktime from.
+ * @return {string} The transaction locktime as an unprefixed hexadecimal
+ *         string.
+ */
 function getTxLocktime(tx) {
   const buffer = bufio.write()
   buffer.writeU32(tx.locktime)
@@ -35,6 +78,12 @@ function getTxLocktime(tx) {
   return toHex(buffer)
 }
 
+/**
+ * @param {any[]} elements A vector of untyped elements.
+ * @return {string} The vector as an unprefixed hexadecimal string, with a
+ *         varint representing vector length followed by the encoded hex version
+ *         of each element.
+ */
 function vectorToRaw(elements) {
   const buffer = bufio.write()
   buffer.writeVarint(elements.length)
@@ -46,8 +95,14 @@ function vectorToRaw(elements) {
   return toHex(buffer)
 }
 
-function toHex(buffer) {
-  return buffer.render().toString("hex")
+/**
+ * @param {StaticWriter | BufferWriter} bufferWriter The bufferWriter to render
+ *        to an unprefixed hex string.
+ * @return {string} The unprefixed hex string representation of the data in the
+ *         passed buffer writer.
+ */
+function toHex(bufferWriter) {
+  return bufferWriter.render().toString("hex")
 }
 
 export const BitcoinTxParser = {
