36f3989e3c6413db40a227cd4f0abfe0d3070476
Jakub Nowakowski <jakub.nowakowski@akena.co>
2020-10-23 17:58:06 +0200

    Export function to convert deposit states
    The function can be used to convert numeric state values returned by Deposit
    contract to a string representation of a state.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import TBTC, { getNetworkIdFromArtifact } from "./src/TBTC.js"
 import BitcoinHelpers from "./src/BitcoinHelpers.js"
 import EthereumHelpers from "./src/EthereumHelpers.js"
+import { nameOfState } from "./src/Deposit.js"
 
-export { BitcoinHelpers, EthereumHelpers, getNetworkIdFromArtifact }
+export {
+  BitcoinHelpers,
+  EthereumHelpers,
+  getNetworkIdFromArtifact,
+  nameOfState
+}
 
 /** @typedef { import("./src/Deposit.js").default } Deposit */
 /** @typedef { import("./src/Redemption.js").default } Redemption */
